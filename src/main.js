c7ae78fe51ec336edc13bb488b75664350923554
Pathomphomg <plug185@gmail.com>
2019-08-21 23:47:54 +0700

    Test search data from json

--- src/main.js
+++ src/main.js
@@ -10,6 +10,13 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 
+import axios from 'axios'
+
+axios.defaults.headers.common['Access-Control-Allow-Headers'] = '*';
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+axios.defaults.headers.get['Accepts'] = 'application/json'
+
 new Vue({
   router,
   store,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,50 @@
 <template>
   <div class="container">
-    <b-table striped hover :items="items"></b-table>
+    <div class="row mb-3">
+      <div class="col-lg-10"><input type="text" class="form-control" aria-label="search" value="" v-model="search"></div>
+      <div class="col-lg-2"><button class="form-control btn btn-primary" @click="searchData">Search</button></div>
+    </div>
+    <div v-if="items.length">
+      <b-table striped hover :items="items"></b-table>
+    </div>
+    <div v-else class="center"> No Data Found</div>
+    {{ items }}
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+// @ is an alias to /src AIzaSyCV3Rukpo1q6KiPKT84BJlXFm00BXWDZSE
 import HelloWorld from '@/components/HelloWorld.vue'
-
+import axios from 'axios'
 export default {
   name: 'home',
   data() {
-      return {
-        items: [
-          { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
-          { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
-          { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
-          { age: 38, first_name: 'Jami', last_name: 'Carney' }
-        ]
-      }
-    },
+    return {
+      search: 'Bangsue',
+      items: [
+      
+      ]
+    }
+  },
   components: {
     HelloWorld
+  },
+  mounted(){
+    if(localStorage.search){
+      this.search = localStorage.search
+    }
+  },
+  methods:{
+    searchData(){
+        /*axios.get('https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+'+this.search+'&key=AIzaSyCV3Rukpo1q6KiPKT84BJlXFm00BXWDZSE')
+          .then(res=> this.items = res )
+          .catch(err => console.log(err))
+        */
+       axios.get('http://localhost:8080/restaurant-ex.json')
+          .then(res=> this.items = res.data.results )
+          .catch(err => console.log(err))
+        localStorage.search = this.search
+    },
   }
 }
 </script>
